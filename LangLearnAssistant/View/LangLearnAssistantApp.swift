d8582ec8820e975ab69580cdd1eda44dacffdcfa
Vlad Krupen <air7360@gmail.com>
2024-09-11 17:46:14 +0300

    TabBarViewCreated

--- LangLearnAssistant/View/LangLearnAssistantApp.swift
+++ LangLearnAssistant/View/LangLearnAssistantApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LangLearnAssistantApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabBarView()
         }
     }
 }
---
+++ LangLearnAssistant/View/TabBarView.swift
@@ -0,0 +1,44 @@
+//
+//  TabBarView.swift
+//  LangLearnAssistant
+//
+//  Created by Vlad on 11.09.24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            Text("List")
+                .tag(1)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.dash")
+                        Text("List")
+                    }
+                }
+            
+            Text("Words")
+                .tag(2)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "textformat.abc")
+                        Text("Words")
+                    }
+                }
+            Text("Links")
+                .tag(3)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "link")
+                        Text("Links")
+                    }
+                }
+        }
+    }
+}
+    
+    #Preview {
+        TabBarView()
+    }
