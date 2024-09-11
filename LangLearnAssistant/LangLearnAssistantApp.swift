8e037f251c69f09c5c83ad2dfaa088b11e84b760
Vlad Krupen <air7360@gmail.com>
2024-09-11 17:36:56 +0300

    Initial commit

---
+++ LangLearnAssistant/LangLearnAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangLearnAssistantApp.swift
+//  LangLearnAssistant
+//
+//  Created by Vlad on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct LangLearnAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
